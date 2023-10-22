e5bbdbdc1bd308919ecee6d92526cd7e55e052a5
jorgeAgoiz <jorgeagoiz@gmail.com>
2023-10-22 18:05:45 +0200

    changes

--- config/nodemailer.ts
+++ config/nodemailer.ts
@@ -7,8 +7,8 @@ import { nodemailer } from "/deps.ts";
 
 export const transporter = nodemailer.createTransport({
   host: EMAIL_HOST,
-  secure: true,
-  port: 465,
+  secure: false,
+  port: 587,
   auth: {
     user: EMAIL_USER,
     pass: EMAIL_PASSWORD,
