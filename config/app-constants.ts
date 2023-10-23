6f62be0559538d5943e34c9fe525494e5f8c288e
jorgeAgoiz <jorgeagoiz@gmail.com>
2023-10-23 19:48:01 +0200

    Changes on environment variables

--- config/app-constants.ts
+++ config/app-constants.ts
@@ -1,9 +1,22 @@
-export const PORT = Deno.env.get("PORT");
-export const AUTH_TOKEN_USERS = Deno.env.get("AUTH_TOKEN_USERS");
-export const AUTH_TOKEN_QUESTIONS = Deno.env.get("AUTH_TOKEN_QUESTIONS");
-export const MONGODB_URI = Deno.env.get("MONGODB_URI");
-export const EMAIL_PASSWORD = Deno.env.get("EMAIL_PASSWORD");
-export const EMAIL_USER = Deno.env.get("EMAIL_USER");
-export const EMAIL_HOST = Deno.env.get("EMAIL_HOST");
+import { load } from "/deps.ts";
+
+const env = await load();
+
+export const PORT = env["PORT"] ?? Deno.env.get("PORT");
+
+export const AUTH_TOKEN_USERS =
+  env["AUTH_TOKEN_USERS"] ?? Deno.env.get("AUTH_TOKEN_USERS");
+
+export const AUTH_TOKEN_QUESTIONS =
+  env["AUTH_TOKEN_QUESTIONS"] ?? Deno.env.get("AUTH_TOKEN_QUESTIONS");
+
+export const MONGODB_URI = env["MONGODB_URI"] ?? Deno.env.get("MONGODB_URI");
+
+export const EMAIL_PASSWORD =
+  env["EMAIL_PASSWORD"] ?? Deno.env.get("EMAIL_PASSWORD");
+
+export const EMAIL_USER = env["EMAIL_USER"] ?? Deno.env.get("EMAIL_USER");
+
+export const EMAIL_HOST = env["EMAIL_HOST"] ?? Deno.env.get("EMAIL_HOST");
 
 export const DUPLICATE_KEY_CODE = "E11000";
